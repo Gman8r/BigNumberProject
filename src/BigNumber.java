cbf0250224cc4fff2ba0d5f24bea3e63409b81ba
Brian Intile <brianintile@gmail.com>
2018-10-15 14:27:20 -0400

    implement addition for positive numbers

--- src/BigNumber.java
+++ src/BigNumber.java
@@ -39,7 +39,37 @@ public class BigNumber
 	//TODO Brian
 	public BigNumber add(BigNumber o)
 	{
-		return null;
+		//TODO Negative number support
+		
+		//Create our iterators and result list
+		LeftRightIterator<Integer> iterator = digits.iterator(DoublyLinkedList.Side.Right);
+		LeftRightIterator<Integer> oIterator = o.digits.iterator(DoublyLinkedList.Side.Right);
+		DoublyLinkedList<Integer> results = new DoublyLinkedList<Integer>();
+		int maxDigits = Math.max(digits.getSize(), o.digits.getSize());		//Determine total iteration size from biggest digit number
+		
+		boolean carry = false;
+		for(int i = 0; i < maxDigits; i++)
+		{
+			//Iterate left and take values if any exist
+			//TODO account for 0 vs 9 in tens complement?
+			int digit = iterator.hasLeft() ? iterator.left() : 0;
+			int oDigit = oIterator.hasLeft() ? oIterator.left() : 0;
+			
+			//Add and account for carrying 1's
+			int digitSum = digit + oDigit + (carry ? 1 : 0);
+			if (digitSum >= 10)
+			{
+				digitSum -= 10;
+				carry = true;
+			}
+			else
+				carry = false;
+			results.addLeft(digitSum);
+		}
+		if (carry)	//Account for final carry
+			results.addLeft(1);	//TODO account for tens complement?
+		
+		return new BigNumber(results);
 	}
 
 	//TODO Brian
